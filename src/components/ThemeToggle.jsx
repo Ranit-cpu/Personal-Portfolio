b57cb04d2e86958dd98e537add19f8a3aea8df4a
Ranit <ranits003@gmail.com>
2025-10-05 19:03:50 +0530

    ThemeToggle Updated

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,16 +1,31 @@
-import { useState} from "react"
-import {Moon, Sun} from "lucide-react";
+import { useEffect, useState } from "react"
+import { Moon, Sun } from "lucide-react";
 
 export const ThemeToggle = ()=> {
     const [isDarkMode, setDarkMode] = useState(false);
 
+    useEffect(()=>{
+        const storedTheme = localStorage.getItem("theme");
+        if(storedTheme === "dark"){
+            setDarkMode(true);
+            document.documentElement.classList.add("dark");
+        }else{
+            localStorage.setItem("theme", "light");
+            setDarkMode(false);
+        }
+    },[])
+
     const toggleDarkMode = () => {
         if (isDarkMode) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
             setDarkMode(false);
         }else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
             setDarkMode(true);
         }
-    }
+    };
 
     return(
         <button onClick={toggleDarkMode}>
